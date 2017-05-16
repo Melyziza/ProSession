e15ab46a3a87b0dc7e0a77cf5ccfe138365e420e
cetecom <cetecom@LC08L5706>
2017-05-16 10:05:52 -0400

    Medio

--- index.php
+++ index.php
@@ -1,17 +1,16 @@
+<?php
+/*session_start();
+$_SESSION["saludo"]="Hola Mundo";*/
+
+?>
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+
 <html>
     <head>
         <meta charset="UTF-8">
         <title></title>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <a href="revision.php">Revisi&oacute;n sesi&oacute;n</a>
     </body>
 </html>
---
+++ revision.php
@@ -0,0 +1,16 @@
+<?php
+$PATH='lib/';
+
+include $PATH.'Conexion.php';
+include $PATH.'Usuario.php';
+
+$oUsr=new Usuario();
+
+$oUsr->nombre='flopez';
+
+if($oUsr->VerificaUsuario())
+
+    echo "Existe";
+    else
+    echo "No existe";    
+
